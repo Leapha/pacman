c6faa8f3e1082570754fdaaa7cf0fa649bb022a7
Jamily M <81268514+Leapha@users.noreply.github.com>
2025-03-24 11:33:40 -0300

    Creating main loop connecting  Menu and Gmane Classes

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import pygame
+from code.Game import Game
 
-print('Setup Stert')
-pygame.init()
-window = pygame.display.set_mode(size = (900,700))
-print('Setup End')
-
-print('Setup Start')
-while True:
-    # check for all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() # Close Window
-            quit() # End Pygame
+game = Game()
+game.run()
